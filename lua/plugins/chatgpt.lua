f3b33c66e2baf054147a83a42084e48fdab08593
Thomas Decroyère <thomas.decroyere@outlook.com>
2023-12-11 05:56:07 +0100

    Update

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -6,6 +6,9 @@ return {
         "nvim-telescope/telescope.nvim"
     },
     event = "VeryLazy",
+    keys = {
+--        { "<leader>cd", ":ChatGPTRun custom-docstring<cr>" }
+    },
     config = function()
         require("chatgpt").setup({
             actions_paths = { debug.getinfo(1, "S").source:sub(2):match("(.*/)") .. "chatgpt-actions.json" },
